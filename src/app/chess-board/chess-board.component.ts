7ee8bfeaa19db22d53238c29029726b1a9c7e38a
Sheehy <Andrew.Sheehy@insight.com>
2022-01-24 10:48:02 -0500

    Completed template for ChessBoard component.

---
+++ src/app/chess-board/chess-board.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chess-board',
+  templateUrl: './chess-board.component.html',
+  styleUrls: ['./chess-board.component.css']
+})
+export class ChessBoardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
