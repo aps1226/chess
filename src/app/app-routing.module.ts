2dfae131fdd44e060ed640532c2e3456e374aba1
Sheehy <Andrew.Sheehy@insight.com>
2022-01-24 10:48:02 -0500

    Completed template for ChessBoard component.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ChessBoardComponent } from './chess-board/chess-board.component';
+
+const routes: Routes = [
+  { path: '', component: ChessBoardComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
