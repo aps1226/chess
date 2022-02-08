6a9ea11c71c3c9b257a4aa9b3b1a8c73151245a0
Sheehy <Andrew.Sheehy@insight.com>
2022-02-08 14:22:16 -0500

    Created apis for users and games databases.

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,35 +1,67 @@
 import express from 'express'
 import path from 'path';
+import sql from 'mssql';
+require('dotenv').config();
 import { ApolloServer } from 'apollo-server-express';
+
 import schema from './graphql/schemasMap';
 
 const app = express();
 const PORT = 8080;
 
-const server = new ApolloServer({
+const config:sql.config = {
+    user: process.env['DB_USERNAME'],
+    password: process.env['DB_PASSWORD'],
+    server: process.env['DB_SERVER'], 
+    database: process.env['DB_NAME'], 
+    pool:{
+      max: 20,
+      min: 10,
+      idleTimeoutMillis: 30000
+    },
+    options: {
+      encrypt: true,
+      trustServerCertificate:true,
+    }
+};
+
+const appPool = new sql.ConnectionPool(config);
+
+const apolloServer = new ApolloServer({
     schema,
 });
 
-server
-    .start()
-        .then(() =>{
-            server.applyMiddleware({
-                app,
-                path:'/graphql'
+appPool.connect()
+    .then((pool) =>{
+        app.locals['db'] = pool;
+
+        app.use(express.static('./src/client/dist/chess/'));
+        app.use(express.json());
+        
+        const usersRouter = require('./routers/usersRouter')(app);
+        app.use('/api/users', usersRouter);
+
+        const gamesRouter = require('./routers/gamesRouter')(app);
+        app.use('/api/games', gamesRouter);
+
+        app.get('/',(req, res) =>{
+            console.log('test');
+            res.render("index.html");
+        });
+
+        apolloServer.start()
+            .then(() =>{
+                apolloServer.applyMiddleware({
+                    app,
+                    path:'/graphql'
+                });
+            })
+            .then(() =>{
+                console.log(`Apollo server is listening at http://localhost:${ PORT }${apolloServer.graphqlPath}.`);
             });
+
+        app.listen(PORT,() => {
+            console.log(`Server listening on http://localhost:${PORT}.`);
         })
-        .then(() =>{
-            console.log(`Apollo server is listening at http://localhost:${ PORT }${server.graphqlPath}.`);
-        })
-
-
-app.use(express.static('./src/client/dist/chess/'));
-
-app.get('/',(req, res) =>{
-    console.log('test');
-    res.render("index.html");
-})
-
-app.listen(PORT,() => {
-    console.log(`Server listening on http://localhost:${PORT}.`);
-})
+    })
+    .catch((e) =>{throw(e)});
---
+++ src/server/routers/usersRouter.ts
@@ -0,0 +1,59 @@
+import express, { Application, RequestHandler } from 'express';
+import userController from '../SQL/usersController';
+
+module.exports = (app:Application) =>{
+
+    const router = express.Router();
+
+    const db = app.locals['db'];
+    
+    // Get all users.
+    router.get('/',
+    userController.allUsers(db),
+    (req,res) =>{
+        const result = JSON.stringify(res["locals"]["queryResult"])
+        res.status(200);
+        res.send(result);
+    });
+
+    // Find user based on userName.
+    router.get('/find',
+    userController.findUser(db),
+    (req,res) =>{
+        const result = JSON.stringify(res["locals"]["queryResult"])
+        res.status(200);
+        res.send(result);
+    });
+
+    // Add new user.
+    router.post('/register',
+    userController.newUser(db),
+    (req,res) =>{
+        const result = JSON.stringify(res["locals"]["queryResult"])
+        res.status(201);
+        res.send(result);
+    });
+
+    // Update a field for an existing user.
+    router.patch('/',
+    userController.findUser(db),
+    userController.updateUser(db),
+    userController.findUser(db),
+    (req,res) =>{
+        const result = JSON.stringify(res["locals"]["queryResult"])
+        res.status(201);
+        res.send(result);
+    });
+
+    // Delete a respective user.
+    router.delete('/',
+    userController.findUser(db),
+    userController.deleteUser(db),
+    (req,res) =>{
+        const result = JSON.stringify(res["locals"]["queryResult"])
+        res.status(204);
+        res.send(result);
+    });
+
+    return router;
+};
